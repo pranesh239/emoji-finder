29bdd809a9379c7c2417ec6f7030faeaace28a61
Pranesh T G <pranesh.g@IN-Praneshg.local>
2019-05-30 09:18:05 +0530

    Updates

---
+++ precache-manifest.1c52cf6d84151ce3af0cb7d564884687.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5068b1a2c90784f11a49f7655d667e78",
+    "url": "/emoji-finder/index.html"
+  },
+  {
+    "revision": "5f3786c02aa48b056a67",
+    "url": "/emoji-finder/static/css/main.b310fb38.chunk.css"
+  },
+  {
+    "revision": "6556664b90fdbab2980a",
+    "url": "/emoji-finder/static/js/2.e924234d.chunk.js"
+  },
+  {
+    "revision": "5f3786c02aa48b056a67",
+    "url": "/emoji-finder/static/js/main.a53e14a2.chunk.js"
+  },
+  {
+    "revision": "6201d940a340361985e8",
+    "url": "/emoji-finder/static/js/runtime~main.482c224c.js"
+  }
+]);
